508a5cb8ab673a9e391724297516189aa1cc9a10
Jonghyeok <jhpmuscle@gmail.com>
2024-01-06 15:31:07 +0900

    fix: change sending data from body to header

--- public/js/api/auth/reissue.js
+++ public/js/api/auth/reissue.js
@@ -2,10 +2,9 @@ async function reissue() {
     await $.ajax({
         type: "POST",
         url: config.authServer + "/api/auth/reissue",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({
-            refreshToken: getCookie("refreshToken")
-        }),
+        headers: {
+            Authorization: "Bearer " + getCookie("refreshToken")
+        },
         success: function (res) {
             setCookie("accessToken", res.accessToken, 2 * 60);
             setCookie("refreshToken", res.refreshToken, 24 * 14 * 60);
