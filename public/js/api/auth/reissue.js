bb1fd299ff483407a1485501a7ca1dd88047f54e
Jonghyeok <jhpmuscle@gmail.com>
2024-04-29 00:43:25 +0900

    !HOTFIX: cookie expiration time fix

--- public/js/api/auth/reissue.js
+++ public/js/api/auth/reissue.js
@@ -8,7 +8,7 @@ async function reissue() {
         success: function (res) {
             setCookie("accessToken", res.accessToken, 2 * 60);
             setCookie("refreshToken", res.refreshToken, 24 * 14 * 60);
-            setCookie('isActive', res.registerStateEnum, accessTime);
+            setCookie('isActive', res.registerStateEnum, 2 * 60);
             if (res.registerStateEnum === "INACTIVE") {
                 location.replace("/register.html");
             } else {
